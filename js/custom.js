c38738061b8a062669434294ad833f411d216e35
rupal kachhwaha <35149577+rupalkachhwaha@users.noreply.github.com>
2020-04-16 22:35:08 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+
+	
+		//$("#portfolio1-startjs").hover(function(){
+ 		//$(".portfolio1-startup").css("height", "137px;");
+  
+		//});
+
+
+		//$("#portfolio1-startjs").hover(function(){
+  		//$(".portfolio1-startup:hover").slideUp(500);
+		//});
+
+
+	$("#portfolio1-startjs").hover(function(){
+	    	$('#portfolio1-hover').show();
+	    	$('#portfolio1-begg').hide();
+	    
+		},function(){
+    		$('.portfolio1-hover').hide();
+	    	$('#portfolio1-begg').show();
+	});
+
+	$("#portfolio1-startjs1").hover(function(){
+	    	$('#portfolio1-hover1').show();
+	    	$('#portfolio1-begg1').hide();
+	    
+		},function(){
+    		$('.portfolio1-hover').hide();
+	    	$('#portfolio1-begg1').show();
+	});
+	$("#portfolio1-startjs2").hover(function(){
+	    	$('#portfolio1-hover2').show();
+	    	$('#portfolio1-begg2').hide();
+	    
+		},function(){
+    		$('.portfolio1-hover').hide();
+	    	$('#portfolio1-begg2').show();
+	});
+
+
+
+	// $("#portfolio1-startjs").hover(function(){
+	// 	$("#portfolio1-startjs").toggle(function(){
+	// 	    $(this).animate({height:40},200);
+	// 	    $('#portfolio1-hover').show();
+	// 	    $('#portfolio1-begg').hide();
+	// 	  },function(){
+	// 	    $(this).animate({height:10},200);
+	// 	    $('.portfolio1-hover').hide();
+	// 	    $('#portfolio1-begg').show();
+	// 	});
+	// });
+	//else {
+		//$('.portfolio1-begg').show();
+	//}
+
+
+});
+
+
+
+	
